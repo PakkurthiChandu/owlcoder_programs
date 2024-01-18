c8e9c90cfedc5f4973b27eddebcd3a332017c053
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2024-01-19 00:17:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rotate a Linked List/rotate-a-linked-list.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution{
+    //Function to rotate a linked list.
+    public Node rotate(Node head, int k) {
+        // add code here
+        Node temp=head,p=head,t=head,t1=head;
+        int c=k,count=0;
+        if(k==0)return head;
+        while(temp!=null)
+        {
+            if(k==1)
+            {
+                t=temp;
+            }
+            t1=temp;
+            temp=temp.next;
+            k--;
+            count++;
+        }
+        if(c==count)return head;
+        head=t.next;
+        t.next=null;
+        t1.next=p;
+        // System.out.println(p.data);
+        return head;
+    }
+}
